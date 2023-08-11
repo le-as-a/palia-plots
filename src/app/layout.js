004ae6433303f220770e880e21bf8280fa6c92e5
le-as-a <leasia0817@gmail.com>
2023-08-10 21:17:12 -0500

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Palia Plots',
+  description: 'An app that helps you optimize your Palia Online farm, planning your plots out before you execute them in game!',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
