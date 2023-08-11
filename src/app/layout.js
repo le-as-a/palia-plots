0f83c8450cfc38892207323af417270b71f0ba17
le-as-a <leasia0817@gmail.com>
2023-08-10 22:23:13 -0500

    set up basic layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,5 @@
+import './globals.css'
+
 export const metadata = {
   title: 'Palia Plots',
   description: 'An app that helps you optimize your Palia Online farm, planning your plots out before you execute them in game!',
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,67 @@
+import styles from './page.module.css';
+
 export default function Home() {
+  const plots = [];
+  function renderPlot(i, image = "") {
+    return (
+      <>
+        <div className={styles.plot} key={`plot${i+1}`}>
+          {image ? image : ""}
+        </div>
+      </>
+    )
+  }
+  for (let i = 0; i < 9; i++) {
+    plots.push(renderPlot(i));
+  }
+
+  const cropInfo = {
+    "Carrot": {
+      ability: "weed prevention",
+      image: "🥕"
+    },
+    "Tomato": {
+      ability: "water retain",
+      image: "🍅"
+    },
+    "Wheat": {
+      ability: "harvest boost",
+      image: "🌾"
+    },
+    "Cotton": {
+      ability: "quality boost",
+      image: "☁️"
+    }
+  }
+
+  const crops = []
+  for (let key in cropInfo) {
+    let info = cropInfo[`${key}`];
+    crops.push((
+      <>
+        <div className={styles.crop}>
+          <div className={styles.crop_image}>
+            {info.image}
+          </div>
+          <div className={styles.crop_info}>
+            <b>{key}</b><br />
+            {info.ability}
+          </div>
+        </div>
+      </>
+    ))
+  }
+
   return (
     <>
-      test
+      <div className={styles.container}>
+        <div className={styles.soil}>
+          {plots}
+        </div>
+        <div className={styles.crops}>
+          {crops}
+        </div>
+      </div>
     </>
   )
 }
